6577ff43caf34f4bf9228b75fd70bdd95aaddab9
David Morgan <itsdavidmorgan@gmail.com>
2017-04-14 20:55:28 -1000

    Added missing styles

--- loop-post.php
+++ loop-post.php
@@ -13,8 +13,8 @@
 <h1 class="headline"><?php the_title(); ?></h1>
 
 <div class="post-author">
-	<p class="align-left"><i class="fa fa-calendar"></i> &nbsp;<?php esc_html_e("Posted on", 'organic-natural'); ?> <?php the_time( esc_html__("F j, Y", 'organic-natural')); ?> <?php esc_html_e("by", 'organic-natural'); ?> <?php esc_url ( the_author_posts_link() ); ?></p>
-	<p class="align-right"><i class="fa fa-comment"></i> &nbsp;<a class="scroll" href="<?php the_permalink(); ?>#comments"><?php comments_number( esc_html__("Leave a Comment", 'organic-natural'), esc_html__("1 Comment", 'organic-natural'), '% Comments'); ?></a></p>
+	<p class="align-left"><i class="fa fa-calendar"></i> &nbsp;<?php esc_html_e("Posted on", 'natural-lite'); ?> <?php the_time( esc_html__("F j, Y", 'natural-lite')); ?> <?php esc_html_e("by", 'natural-lite'); ?> <?php esc_url ( the_author_posts_link() ); ?></p>
+	<p class="align-right"><i class="fa fa-comment"></i> &nbsp;<a class="scroll" href="<?php the_permalink(); ?>#comments"><?php comments_number( esc_html__("Leave a Comment", 'natural-lite'), esc_html__("1 Comment", 'natural-lite'), '% Comments'); ?></a></p>
 </div>
 
 <?php if ( has_post_thumbnail() ) { ?>
@@ -27,22 +27,22 @@
 <?php the_content(); ?>
 
 <?php wp_link_pages(array(
-	'before' => '<p class="page-links"><span class="link-label">' . esc_html__('Pages:', 'organic-natural') . '</span>',
+	'before' => '<p class="page-links"><span class="link-label">' . esc_html__('Pages:', 'natural-lite') . '</span>',
 	'after' => '</p>',
 	'link_before' => '<span>',
 	'link_after' => '</span>',
 	'next_or_number' => 'next_and_number',
-	'nextpagelink' => esc_html__('Next', 'organic-natural'),
-	'previouspagelink' => esc_html__('Previous', 'organic-natural'),
+	'nextpagelink' => esc_html__('Next', 'natural-lite'),
+	'previouspagelink' => esc_html__('Previous', 'natural-lite'),
 	'pagelink' => '%',
 	'echo' => 1 )
 ); ?>
 
-<?php edit_post_link( esc_html__("(Edit)", 'organic-natural'), '', ''); ?>
+<?php edit_post_link( esc_html__("(Edit)", 'natural-lite'), '', ''); ?>
 
 <!-- BEGIN .post-meta -->
 <div class="post-meta radius-full">
-	<p><i class="fa fa-reorder"></i> &nbsp;<?php esc_html_e("Category:", 'organic-natural'); ?> <?php the_category(', '); ?> <?php $tag_list = get_the_tag_list( esc_html__( ", ", 'organic-natural' ) ); if ( ! empty( $tag_list ) ) { ?> &nbsp; &nbsp; <i class="fa fa-tags"></i> &nbsp;<?php esc_html_e("Tags:", 'organic-natural'); ?> <?php the_tags(''); } ?></p>
+	<p><i class="fa fa-reorder"></i> &nbsp;<?php esc_html_e("Category:", 'natural-lite'); ?> <?php the_category(', '); ?> <?php $tag_list = get_the_tag_list( esc_html__( ", ", 'natural-lite' ) ); if ( ! empty( $tag_list ) ) { ?> &nbsp; &nbsp; <i class="fa fa-tags"></i> &nbsp;<?php esc_html_e("Tags:", 'natural-lite'); ?> <?php the_tags(''); } ?></p>
 <!-- END .post-meta -->
 </div>
 
@@ -59,7 +59,7 @@
 
 <?php endwhile; else: ?>
 
-<p><?php esc_html_e("Sorry, no posts matched your criteria.", 'organic-natural'); ?></p>
+<p><?php esc_html_e("Sorry, no posts matched your criteria.", 'natural-lite'); ?></p>
 
 <?php endif; ?>
 
